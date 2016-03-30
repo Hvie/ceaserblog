49646b358eda103f3ccca22122791ff576337819
Hvie <viehv@outlook.com>
2016-03-30 21:16:39 +0800

    user register function done

--- www/handlers.py
+++ www/handlers.py
@@ -5,6 +5,12 @@
 import re, time, json, logging, hashlib, base64, asyncio
 from coroweb import get, post
 from models import User, Comment, Blog, next_id
+from aiohttp import web
+from config import configs
+from apis import APIValueError, APIResourceNotFoundError, APIPermissionError
+
+COOKIE_NAME = 'Uly'
+
 '''
 @get('/')
 @asyncio.coroutine
@@ -29,7 +35,7 @@ def index(request):
         '__template__': 'blogs.html',
         'blogs': blogs
     }
-
+'''
 @get('/api/users')
 def api_get_users():
     users = yield from User.findAll(orderBy='created_at desc')
@@ -39,4 +45,32 @@ def api_get_users():
         print(u.passwd)
         # u.passwd = "******"
     return dict(users=users)
+'''
+_RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
+_RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
 
+@post('/api/users')
+def api_register_user(*, email, name, passwd):
+    if not name or not name.strip():
+        raise APIValueError('name')
+    if not email or not _RE_EMAIL.match(email):
+        raise APIValueError('email')
+    if not passwd or not _RE_SHA1.match(passwd):
+        raise APIValueError('passwd')
+    users = yield from User.findAll('email=?', [email])
+    if len(users) > 0:
+        raise APIValueError('register:failed', 'email', 'Email is already in use.')
+    uid = next_id()
+    sha1_passwd = '%s:%s' % (uid, passwd)
+    user = User(id=uid, name=name.strip(), email=email, passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(),
+                 image='http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(email.encode('utf-8')).hexdigest())
+    yield from user.save()
+
+    # make session cookie:
+    r = web.Response()
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+    # I will finish user2cookie function later
+    user.passwd = '******'
+    r.content_type = 'application/json'
+    r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
+    return r
